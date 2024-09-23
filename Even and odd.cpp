80843b8461497bdd2cbeda07c7a8816f767820f9
UndeShravani <shravani.unde24@sanjivani.edu.in>
2024-09-24 00:47:27 +0530

    Add files via upload

---
+++ Even and odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+  cout<<"Enetr a number :";
+  cin>>a;
+  if(a%2==0){
+    cout<<"The given number is EVEN "<<endl;
+  }else{
+    cout<<"The given number is ODD "<<endl;
+  }
+  return 0;
+}
